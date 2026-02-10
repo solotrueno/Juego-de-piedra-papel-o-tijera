59e7c09ccb0ae226dccabc85f2a50c96237f8faa
Desarrollador Java Script <jhonson123456789castro@gmail.com>
2026-02-09 20:32:03 -0500

    Add Principal class with main method to run game

---
+++ principal.java
@@ -0,0 +1,6 @@
+ class Principal {
+    public static void main(String[] args) {
+        Juego juego = new Juego(); // Creamos el objeto juego
+        juego.jugar();             // Ejecutamos el juego
+    }
+}
